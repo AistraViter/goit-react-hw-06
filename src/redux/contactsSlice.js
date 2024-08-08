2593fea5a4c9dc6d220ab7381898773a0168e5b5
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-08-08 18:44:42 +0300

    redux-persist added

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -9,7 +9,6 @@ const contactsSlice = createSlice({
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    filter: "",
   },
   reducers: {
     addContact: {
